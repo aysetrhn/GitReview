00a67829db66d93bd408321dfaaf46535d2bda4c
trhnays3 <trhnays3@gmail.com>
2020-09-30 18:50:19 +0300

    feature 3.satir

--- src/Ders1.java
+++ src/Ders1.java
@@ -5,7 +5,9 @@ public class Ders1 {
 		
 		System.out.println("Saat 17:52");
 		System.out.println("Saat 18:46");	
-		
+		System.out.println("Saat 18:05 - feature branch 1. satir");
+		System.out.println("Saat 18:12 - feature branch 2. satir");
+		System.out.println("Saat 18:48 - feature branch 3. satir");
 	}
 	
 }
